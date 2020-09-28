7a66bc4ecce1e45b6434c4775ee364e1082f7a49
Tzahi <tzahi@fortify500.com>
2020-09-28 11:57:54 +0300

    dao
    Signed-off-by: Tzahi <tzahi@fortify500.com>

---
+++ dao/run.go
@@ -0,0 +1,115 @@
+/*
+Copyright © 2020 stepsman authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package dao
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+type RunStatusType int64
+
+const (
+	RunStopped    RunStatusType = 10
+	RunInProgress RunStatusType = 12
+	RunDone       RunStatusType = 15
+)
+
+type RunRecord struct {
+	Id     int64
+	UUID   string
+	Title  string
+	Cursor int64
+	Status RunStatusType
+	Script string
+}
+
+func ListRuns() ([]*RunRecord, error) {
+	var result []*RunRecord
+	rows, err := DB.SQL().Queryx("SELECT * FROM runs ORDER BY id DESC")
+	if err != nil {
+		return nil, fmt.Errorf("failed to query database runs table: %w", err)
+	}
+
+	for rows.Next() {
+		var run RunRecord
+		err = rows.StructScan(&run)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse database runs row: %w", err)
+		}
+		result = append(result, &run)
+	}
+	return result, nil
+}
+
+func GetRun(runId int64) (*RunRecord, error) {
+	var result *RunRecord
+	rows, err := DB.SQL().Queryx("SELECT * FROM runs where id=$1", runId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query database runs table - get: %w", err)
+	}
+
+	for rows.Next() {
+		var run RunRecord
+		err = rows.StructScan(&run)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse database runs row - get: %w", err)
+		}
+		result = &run
+	}
+	if result == nil {
+		return nil, ErrRecordNotFound
+	}
+	return result, nil
+}
+
+func GetTitleInProgressTx(tx *sqlx.Tx, count int, title string) error {
+	return tx.Get(&count, "SELECT count(*) FROM runs where status=$1 and title=$2", RunInProgress, title)
+}
+
+func UpdateRunStatus(runId int64, newStatus RunStatusType) (sql.Result, error) {
+	return DB.SQL().Exec("update runs set status=$1 where id=$2", newStatus, runId)
+}
+
+func UpdateRunCursorTx(tx *sqlx.Tx, stepId int64, runId int64) (sql.Result, error) {
+	return tx.Exec("update runs set cursor=$1 where id=$2", stepId, runId)
+}
+
+func TranslateToRunStatus(status string) (RunStatusType, error) {
+	switch status {
+	case "Stopped":
+		return RunStopped, nil
+	case "In Progress":
+		return RunInProgress, nil
+	case "Done":
+		return RunDone, nil
+	default:
+		return RunStopped, fmt.Errorf("failed to translate run status: %s", status)
+	}
+}
+func (s RunStatusType) TranslateRunStatus() (string, error) {
+	switch s {
+	case RunStopped:
+		return "Stopped", nil
+	case RunInProgress:
+		return "In Progress", nil
+	case RunDone:
+		return "Done", nil
+	default:
+		return "", fmt.Errorf("failed to translate run status: %d", s)
+	}
+}
---
+++ dao/step.go
@@ -0,0 +1,123 @@
+/*
+Copyright © 2020 stepsman authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package dao
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+type StepStatusType int64
+
+const StepNotStarted StepStatusType = 0
+
+const StepInProgress StepStatusType = 2
+
+const StepCanceled StepStatusType = 3
+
+const StepFailed StepStatusType = 4
+
+const StepSkipped StepStatusType = 6
+
+type StepRecord struct {
+	RunId     int64 `db:"run_id"`
+	StepId    int64 `db:"step_id"`
+	UUID      string
+	Name      string
+	Status    StepStatusType
+	HeartBeat int64
+	Script    string
+}
+
+func CreateStepTx(tx *sqlx.Tx, stepRecord *StepRecord) (sql.Result, error) {
+	return tx.NamedExec("INSERT INTO steps(run_id, step_id, uuid, name, status, heartbeat, script) values(:run_id,:step_id,:uuid,:name,:status,0,:script)", stepRecord)
+}
+
+func (s *StepRecord) UpdateHeartBeat() error {
+	_, err := DB.SQL().Exec("update steps set UpdateHeheartbeat=$1 where run_id=$2 and step_id=$3", time.Now().Unix(), s.RunId, s.StepId)
+	if err != nil {
+		return fmt.Errorf("failed to update database step heartbeat: %w", err)
+	}
+	return nil
+}
+
+func GetStep(runId int64, stepId int64) (*StepRecord, error) {
+	return GetStepTx(nil, runId, stepId)
+}
+func GetStepTx(tx *sqlx.Tx, runId int64, stepId int64) (*StepRecord, error) {
+	var result *StepRecord
+
+	const query = "SELECT * FROM steps where run_id=$1 and step_id=$2"
+	var rows *sqlx.Rows
+	var err error
+	if tx == nil {
+		rows, err = DB.SQL().Queryx(query, runId, stepId)
+	} else {
+		rows, err = tx.Queryx(query, runId, stepId)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to query database steps table - get: %w", err)
+	}
+
+	for rows.Next() {
+		var step StepRecord
+		err = rows.StructScan(&step)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse database steps row - get: %w", err)
+		}
+		result = &step
+	}
+	if result == nil {
+		return nil, ErrRecordNotFound
+	}
+	return result, nil
+}
+
+func ListSteps(runId int64) ([]*StepRecord, error) {
+	return ListStepsTx(nil, runId)
+}
+
+func ListStepsTx(tx *sqlx.Tx, runId int64) ([]*StepRecord, error) {
+	var result []*StepRecord
+	var rows *sqlx.Rows
+	var err error
+	const query = "SELECT * FROM steps WHERE run_id=$1"
+	if tx == nil {
+		rows, err = DB.SQL().Queryx(query, runId)
+	} else {
+		rows, err = tx.Queryx(query, runId)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to query database steps table: %w", err)
+	}
+
+	for rows.Next() {
+		var step StepRecord
+		err = rows.StructScan(&step)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse database steps row: %w", err)
+		}
+		result = append(result, &step)
+	}
+	return result, nil
+}
+
+func (s *StepRecord) UpdateStatusAndHeartBeatTx(tx *sqlx.Tx, newStatus StepStatusType, heartBeat int64) (sql.Result, error) {
+	return tx.Exec("update steps set status=$1, heartbeat=$2 where run_id=$3 and step_id=$4", newStatus, heartBeat, s.RunId, s.StepId)
+}
